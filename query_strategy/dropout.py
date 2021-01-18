e6e4b46588b9a1668d392a88c17859f4b1335aa8
Stefan Ott <kicker17@gmx.de>
2021-01-18 13:46:40 +0100

    BALD divergence implementation

--- query_strategy/dropout.py
+++ query_strategy/dropout.py
@@ -5,6 +5,7 @@ from sklearn.exceptions import NotFittedError
 from sklearn.base import BaseEstimator
 
 from modAL.utils.data import modALinput
+from modAL.uncertainty import _proba_entropy
 import sys
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -35,12 +36,31 @@ def mc_dropout(classifier: BaseEstimator, X, num_cycles : int = 50,
     set_dropout_mode(classifier.estimator.module_, train_mode=False)
 
     #TODO: implement querye selection measure (e.g. BALD (Bayesian active learning divergence))
+    bald = _bald_divergence(predictions)
 
     #TODO: format selected query as modAL tuple to fix current data format issues
 
     #to inspect MC Dropout forward passes you can set a breakpoint here
     return pred_list
 
+
+def _bald_divergence(proba) -> np.ndarray:
+    accumulated_score = np.zeros(shape=proba[0].shape)
+    accumulated_entropy = np.zeros(shape=(proba[0].shape[0], ))
+
+    for dropout_cycle in proba:
+        accumulated_score += proba[dropout_cycle]
+        accumulated_entropy += _proba_entropy(proba[dropout_cycle])
+    
+    # average inter score entropy
+    f_x = accumulated_entropy / len(proba)
+    
+    # average entropy in scores
+    average_score = accumulated_score / len(proba)
+    g_x = _proba_entropy(average_score)
+    
+    u_x = g_x - f_x
+    return u_x
 
 def set_dropout_mode(model, train_mode: bool):
     """ Function to enable the dropout layers by setting them to user specified mode (bool: train_mode)"""
