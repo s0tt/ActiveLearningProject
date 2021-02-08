49f3702fea135c1e2af9c300f7a5cbf0f20cabc4
Michael <34004884+Schweinebaermann@users.noreply.github.com>
2021-02-08 01:29:11 +0100

    Annotation Framework adaptation to metrics and a new operating concept
    - Views with filters and customized buttons for the Active Learner.
    - Restructuring and simplification of export and code.
    - Possibility to label several instances and to include (several) metrics. Only the labeled instances are returned.

--- Annotation_Interface/Labeling.py
+++ Annotation_Interface/Labeling.py
@@ -2,61 +2,85 @@ import requests
 import json
 
 
-def label(question, text):
+class mydict(dict):
+    def __str__(self):
+        return json.dumps(self)
+
+    def __repr__(self):
+        return json.dumps(self)
+
+def extractData(dataDict):
+    results = []
+    for completion in dataDict["completions"]:
+        resultList = completion["result"]
+        charSpans = []
+        answerTexts = []
+        weighting=-1
+    
+        for result in resultList:
+            if result["from_name"]=="answer":
+                # With the charspans, the end index is the last character that is inclusive
+                charSpans.append((result["value"]["start"], result["value"]["end"]-1))
+                answerTexts.append(result["value"]["text"])
+            elif  result["from_name"]=="weighting":
+                weighting=result["value"]["rating"]
+    
+        if len(answerTexts)<1:
+                return None
+    
+        resultDict = {}
+        resultDict["charSpans"] = charSpans
+        resultDict["texts"] = answerTexts
+        if weighting>=0:
+            resultDict["weighting"] = weighting
+        results.append(resultDict)
+    return results[0]
+
+def label(instances):
     """
-    Method sends the question and the text for labeling to the annotation framework and returns a dictionary with the text and the char spans. If necessary, there is also a weighting.
+    Method sends the question and the text for labeling to the annotation framework and returns a list dictionaries with the text and the char spans. If necessary, there is also a weighting.
     Important: The Label-Studio server must be running with the correct configuration (the "questionAnswering" project already has the right one) and for a port other than 8080, the port variable must be changed manually.
 
-    :param question: Question which should be answered
-    :param text: Text from which the answer to the question should be extracted
-    :return: Dictionary containing a list of char spans and texts. A weighting is optionally specified
-             e.g. {'charSpans': [(92, 125), (156, 168)], 'texts': ['a golden statue of the Virgin Mary', 'Main Building'], 'weighting': 4}
+    :param instances: List of entities to be labeled, consisting of a list with text, question and a dictionary of metrics e.g.  [[text, query, {"metric_1":4, "metric_2":2}],[text2, query2,{"metric_1":3, "metric_2":3}]
+    :return: List of Dictionaries containing a list of char spans and texts. A weighting is optionally specified
+             e.g. [{'charSpans': [(92, 125), (156, 168)], 'texts': ['a golden statue of the Virgin Mary', 'Main Building'], 'weighting': 4},...]
     """ 
     
     port = "8080"
+    
+    
+    importList = []
+    for instance in instances:
+        couples = [["text",instance[0].replace('"','#$@')],
+               ["question",instance[1].replace('"','#$@')]]
+        
+        for key,value in instance[2].items():
+            couples.append([key, str(value)])
 
-    # Quotation marks are misinterpreted in request
-    editText = text.replace('"','#$@')
+        pairs =  mydict(couples)
+        importList.append(pairs)
+
     
     headers = {
         'Content-Type': 'application/json',
     }
-    data = '[{"text": "' + editText + '", "question": "' + question + '"}]'
-    
+    data = str(importList)
+  
     # Sends the request to Label Studio with the text to be annotated
-    response = requests.post('http://localhost:'+port+'/api/project/sendTask', headers=headers, data=data.encode('utf-8'))
+    response = requests.post('http://localhost:'+port+'/api/project/sendTask', headers=headers, data=data)
     
     if not response.ok:
         print("Something went wrong")
         return None
     
-    response = requests.get('http://localhost:'+port+'/api/project/getLabel')
+    response = requests.get('http://localhost:'+port+'/api/project/getLabels')
 
     if not response.ok:
         print("Something went wrong")
         return None
 
-    responseDict = json.loads(response.content)
-    resultList = responseDict["completions"][0]["result"]
-    charSpans = []
-    answerTexts = []
-    weighting=-1
-
-    for result in resultList:
-        if result["from_name"]=="answer":
-            # With the charspans, the end index is the last character that is inclusive
-            charSpans.append((result["value"]["start"], result["value"]["end"]-1))
-            answerTexts.append(result["value"]["text"])
-        elif  result["from_name"]=="weighting":
-            weighting=result["value"]["rating"]
-
-    if len(answerTexts)<1:
-            return None
-
-    resultDict = {}
-    resultDict["charSpans"] = charSpans
-    resultDict["texts"] = answerTexts
-    if weighting>=0:
-        resultDict["weighting"] = weighting
-
-    return resultDict
+    responseList = json.loads(response.content)
+    resultList = []
+    for dataDict in responseList:
+        resultList.append(extractData(dataDict))
+    return resultList
